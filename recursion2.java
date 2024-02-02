7728df87de89970f18d474c6cbda9bfa2a28a3d2
SOUMADEEP CHAKRABORTY <109031209+RAZE24003@users.noreply.github.com>
2024-02-02 23:46:36 +0530

    Add files via upload

---
+++ recursion2.java
@@ -0,0 +1,18 @@
+public class recursion2 {
+     public static void printnum(int n) {
+
+        if(n>5)
+        {
+            return;
+        }
+        System.out.println(n);
+        printnum(n+1);
+        
+    }
+   
+    public static void main(String[] args) {
+        int n=1;
+        printnum(n);
+    }
+    
+}
